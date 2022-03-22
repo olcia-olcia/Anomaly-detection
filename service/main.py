bf3536c549a443def89ac761dd7e62b1b6180796
Aleksandra M <olcciaa5@gmail.com>
2022-03-22 23:45:50 +0100

    adding monitoring app into fast api

--- service/main.py
+++ service/main.py
@@ -1,11 +1,20 @@
+import re
 from joblib import load
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
-
+from prometheus_client import make_asgi_app, Counter, Histogram
+import time
 
 app = FastAPI()
+app_prom = make_asgi_app()
+app.mount("/metrics", app_prom)
 clf = load('anomaly.joblib') 
+
+pred_counter = Counter('predictions', 'Number of predictions')
+predictions_output_hist = Histogram('predictions_output', 'Predictions output')
+predictions_scores_hist = Histogram('predictions_scores', 'Predictions scores')
+predictions_latency_hist = Histogram('predictions_latency', 'Latency of predictions')
 
 class PredictionRequest(BaseModel):
     feature_vector: List[float]
@@ -13,7 +22,8 @@ class PredictionRequest(BaseModel):
 
 @app.post("/prediction")
 def predict(request: PredictionRequest):
-
+    start_time = time.time()
+    pred_counter.inc()
     prediction = clf.predict([request.feature_vector])
     response = {"is_inlier": int(prediction[0])}
 
@@ -22,11 +32,18 @@ def predict(request: PredictionRequest):
         scores = clf.score_samples([request.feature_vector])
         response['anomaly_score'] = scores[0]
 
+    response['time'] = time.time() - start_time
+
+    predictions_output_hist.observe(response['is_inliner'])
+    predictions_scores_hist.observe(response['anomaly_score'])
+    predictions_latency_hist.observe(response['time'])
+
     return response
 
 @app.get("/model_information")
 def model_information():
     return clf.get_params()
+
 
 
 if __name__ == "__main__":
