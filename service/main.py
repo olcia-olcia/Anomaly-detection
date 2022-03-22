2fccc9b58006481a6b8d4200a5473bf041e2f75f
Aleksandra M <olcciaa5@gmail.com>
2022-03-23 00:35:03 +0100

    main file update

--- service/main.py
+++ service/main.py
@@ -11,37 +11,46 @@ app_prom = make_asgi_app()
 app.mount("/metrics", app_prom)
 clf = load('anomaly.joblib') 
 
-pred_counter = Counter('predictions', 'Number of predictions')
+predictions_counter = Counter('predictions', 'Number of predictions')
+model_info_counter = Counter(
+    'model_info', 'Number of executions of model information')
 predictions_output_hist = Histogram('predictions_output', 'Predictions output')
 predictions_scores_hist = Histogram('predictions_scores', 'Predictions scores')
-predictions_latency_hist = Histogram('predictions_latency', 'Latency of predictions')
+predictions_latency_hist = Histogram(
+    'predictions_latency', 'Latency of predictions')
 
 class PredictionRequest(BaseModel):
     feature_vector: List[float]
     score: bool
 
+
+class PredictionResponse(BaseModel):
+    score: float
+
 @app.post("/prediction")
 def predict(request: PredictionRequest):
     start_time = time.time()
-    pred_counter.inc()
+    predictions_counter.inc()
+
     prediction = clf.predict([request.feature_vector])
     response = {"is_inlier": int(prediction[0])}
+    predictions_output_hist.observe(int(0))
 
     if request.score:
 
         scores = clf.score_samples([request.feature_vector])
         response['anomaly_score'] = scores[0]
+        predictions_scores_hist.observe(scores[0])
 
-    response['time'] = time.time() - start_time
-
-    predictions_output_hist.observe(response['is_inliner'])
-    predictions_scores_hist.observe(response['anomaly_score'])
-    predictions_latency_hist.observe(response['time'])
+    delta = time.time() - start_time    
+    response['time'] = delta
+    predictions_latency_hist.observe(delta)
 
     return response
 
 @app.get("/model_information")
 def model_information():
+    model_info_counter.inc()
     return clf.get_params()
 
 
